1b039cc69e61a2b1dc186bd67191b98148635764
FirstZverev <46249639+FirstZverev@users.noreply.github.com>
2020-04-16 10:26:26 +0300

    Initial Commit

---
+++ LessonOneObjcC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LessonOneObjcC
+//
+//  Created by Володя Зверев on 16.04.2020.
+//  Copyright © 2020 zverev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
